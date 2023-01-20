fc8caa979c1f649e557acc932696e0af83a257cf
DanielBudusan <danibudusan@gmmail.com>
2023-01-20 20:43:39 +0200

    s

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ import { updateCactus, setupCactus, getCactusRects } from './cactus.js'
 
 const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
-const SPEED_SCALE_INCREASE = 0.001;
+const SPEED_SCALE_INCREASE = 0.1;
 
 const worldElem = document.querySelector('[data-world]')
 const scoreElem = document.querySelector('[data-score]')
